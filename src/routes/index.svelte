0c1cfb37875a7d3df89a74b1a8ae1ec77301d67a
Christ.I.An <webmaster@zwergius.com>
2020-09-10 17:39:21 +0200

    Adds index to get service-worker working, changes redirect code to 303

---
+++ src/routes/index.svelte
@@ -0,0 +1,6 @@
+<script context="module">
+  // This file is needed otherwise service-worker-index.html won't be added to build'
+  export async function preload(page, { language }) {
+    return this.redirect(303, language)
+  }
+</script>
