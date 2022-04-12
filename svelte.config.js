ef187d4072d5a23580bd3587219273b9e344252c
zwergius <webmaster@zwergius.com>
2022-04-12 10:29:29 +0200

    Adds 404 fallback

--- svelte.config.js
+++ svelte.config.js
@@ -22,12 +22,14 @@ const config = {
     // By default, `npm run build` will create a standard Node app.
     // You can create optimized builds for different platforms by
     // specifying a different adapter
-    adapter: adapter(),
+    adapter: adapter({
+      fallback: '404.html'
+    }),
     prerender: {
       crawl: true,
       default: true,
       enabled: true,
-      // onError: handleError,
+      onError: handleError,
       entries: [
         '/da',
         '/es',
