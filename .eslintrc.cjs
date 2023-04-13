1667762a9c9e7a512721193463b7321ee35d68a4
Javitocatral <javitocatral@gmail.com>
2023-04-13 14:28:48 +0200

    fallo

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,81 +1,54 @@
-const typescript = require('typescript')
-
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['svelte3', '@typescript-eslint'],
+  ignorePatterns: ['*.cjs'],
+  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+  settings: {
+    'svelte3/typescript': () => require('typescript'),
+  },
   parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
-    extraFileExtensions: ['.svelte'],
     sourceType: 'module',
     ecmaVersion: 2020,
   },
-  rules: {
-    semi: ['error', 'always'],
-    'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
-    'tsdoc/syntax': 'warn',
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-    'import/prefer-default-export': 'off',
-    'import/no-mutable-exports': 0,
-    'no-labels': 0,
-    'no-restricted-syntax': 0,
-  },
-  overrides: [
-    {
-      files: ['**/*.svelte'],
-      processor: 'svelte3/svelte3',
-      rules: {
-        'import/no-unresolved': 'off',
-        'import/first': 'off',
-        'import/no-duplicates': 'off',
-        'import/no-mutable-exports': 'off',
-        'import/extensions': 'off',
-        '@typescript-eslint/no-unsafe-member-access': 'off',
-        '@typescript-eslint/no-unsafe-argument': 'off',
-        '@typescript-eslint/restrict-template-expressions': [
-          'warn',
-          {
-            allowNumber: true,
-            allowBoolean: true,
-            allowNullish: true,
-            allowAny: true,
-          },
-        ],
-      },
-    },
-  ],
-  settings: {
-    'svelte3/typescript': () => typescript,
-    'svelte3/ignore-styles': () => true,
-    'import/extensions': ['.js', '.ts', '.svelte'],
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.ts'],
-      },
-      typescript: {},
-    },
-  },
-  plugins: ['svelte3', '@typescript-eslint', 'eslint-plugin-tsdoc'],
-  extends: [
-    'airbnb-base',
-    'airbnb-typescript/base',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:eslint-comments/recommended',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
-    'prettier',
-  ],
-  ignorePatterns: [
-    '*.cjs',
-    'static/*.js',
-    'svelte.config.js',
-    'scripts/js/*.ts',
-  ],
   env: {
     browser: true,
+    es2017: true,
     node: true,
-    es2020: true,
+  },
+  rules: {
+    'array-callback-return': 'error',
+    'prefer-destructuring': 'error',
+    'no-return-await': 'error',
+    'require-await': 'error',
+    'no-undef': 'off',
+    'no-var': 'error',
+    'prefer-template': 'error',
+    'no-else-return': 'error',
+    'template-curly-spacing': 'error',
+    'one-var': ['error', 'never'],
+    eqeqeq: 'error',
+    'object-shorthand': 'error',
+    'no-unneeded-ternary': 'error',
+    'quote-props': ['error', 'as-needed'],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', ignoreRestSiblings: true },
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        types: {
+          Function: false,
+        },
+      },
+    ],
   },
 }
