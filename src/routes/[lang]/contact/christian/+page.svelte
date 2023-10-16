f39f3843867fe54145788bb2f5a3aeae524e45e6
Javitocatral <javitocatral@gmail.com>
2023-10-16 16:03:11 +0200

    fix pr comment

--- src/routes/[lang]/contact/christian/+page.svelte
+++ src/routes/[lang]/contact/christian/+page.svelte
@@ -24,46 +24,46 @@
   }
 </script>
 
-<div class="container-links">
-  <a rel="external" href={pkpass}
-    ><img
-      class="logo-wallet"
+<div class="container">
+  <a rel="external" href={pkpass}>
+    <img
+      class="wallet-log"
       src={iosWallet}
       alt="add the card to your wallet for ios"
-    /></a
-  >
-  <a rel="external" target="_blank" href={passGoogle}
-    ><img
-      class="logo-wallet"
+    />
+  </a>
+  <a rel="external" target="_blank" href={passGoogle}>
+    <img
+      class="wallet-logo"
       src={googleWallet}
       alt="add the card to your wallet for android"
-    /></a
-  >
+    />
+  </a>
 </div>
 
 <style>
-  .container-links {
+  .container {
     gap: 50px;
     width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
   }
-  .logo-wallet {
+  .wallet-logo {
     width: 320px;
     height: auto;
   }
   @media only screen and (max-width: 1024px) {
-    .container-links {
+    .container {
       flex-direction: column;
       padding-top: var(--header-height);
     }
-    .logo-wallet {
+    .wallet-logo {
       width: 280px;
     }
   }
   @media only screen and (max-width: 767px) {
-    .logo-wallet {
+    .wallet-logo {
       width: 200px;
     }
   }
