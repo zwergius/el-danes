928c62dccfa2d5fd05a99a2d6a38d6b9024c1e40
zwergius <webmaster@zwergius.com>
2024-08-15 18:59:27 +0200

    minor fix

--- src/lib/stores/theme.ts
+++ src/lib/stores/theme.ts
@@ -26,10 +26,12 @@ var themeColors = {
   dark: [
     { name: '--text', value: '#fff' },
     { name: '--background', value: '#000' },
+    { name: '--overlay', value: 'rgb(0, 0, 0, 0.8)' },
   ],
   light: [
     { name: '--text', value: '#000' },
     { name: '--background', value: '#fff' },
+    { name: '--overlay', value: 'rgb(255, 255, 255, 0.8)' },
   ],
 }
 
